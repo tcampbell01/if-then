ae08e6e64b6c0b78f7b157d505c6fd2f0cf29078
tcampbell01 <teresa.j.campbell@gmail.com>
2022-08-19 15:53:52 -0400

    code blocks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("and I am afraid of aliens!");
+        }
     }
 }
